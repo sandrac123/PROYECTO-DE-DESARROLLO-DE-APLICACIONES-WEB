71ae81a842e3582b58c1485bc30cd029d9a2186d
sandrac123 <130832579+sandrac123@users.noreply.github.com>
2025-06-03 12:39:40 +0200

    cambios

--- uno.js
+++ uno.js
@@ -1,6 +1,6 @@
 const encuestas = [
     { id: 1, nombre: "Encuesta de Hábitos de consumo"},
-    { id: 2, nombre: "Ecuesta Premium"},
+    { id: 2, nombre: "Encuesta Premium"},
     { id: 3, nombre: "Valoracion del curso"},
   ];
   
